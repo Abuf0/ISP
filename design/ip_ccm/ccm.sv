1b0d7a30a6c947f13969450c7592ee9497c4d41b
Abu <1370508435@qq.com>
2024-08-14 15:44:06 +0800

    add ccm model & rtl

---
+++ design/ip_ccm/ccm.sv
@@ -0,0 +1,100 @@
+module ccm#(
+    parameter DW = 16   ,
+    parameter H = 1280  ,
+    parameter V = 720   ,
+    parameter HW = 11   ,
+    parameter VW = 10
+)(
+    input                   clk                   ,
+    input                   rstn                  ,
+    input                   ccm_en                ,
+    input        [DW-1:0]   ccm_coef_r [0:3]      ,
+    input        [DW-1:0]   ccm_coef_g [0:3]      ,
+    input        [DW-1:0]   ccm_coef_b [0:3]      ,
+    input                   pixel_data_in_vld     , 
+    input        [DW-1:0]   pixel_data_in_r       ,
+    input        [DW-1:0]   pixel_data_in_g       ,
+    input        [DW-1:0]   pixel_data_in_b       ,
+    output logic            pixel_data_out_vld    ,
+    output logic [DW-1:0]   pixel_data_out_r      ,
+    output logic [DW-1:0]   pixel_data_out_g      ,
+    output logic [DW-1:0]   pixel_data_out_b      ,
+    output logic            ccm_done        
+);
+
+logic [DW-1:0]   pixel_data_r;          
+logic [DW-1:0]   pixel_data_g;          
+logic [DW-1:0]   pixel_data_b;  
+
+logic [DW+DW+2-1:0] pixel_data_out_r_tmp;
+logic [DW+DW+2-1:0] pixel_data_out_g_tmp;
+logic [DW+DW+2-1:0] pixel_data_out_b_tmp;
+
+logic [HW-1:0] h_cnt; 
+logic [VW-1:0] v_cnt; 
+
+logic pixel_data_out_vld_pre;
+
+always_ff@(posedge clk or negedge rstn) begin
+    if(~rstn) begin
+        pixel_data_r <= 'd0;
+        pixel_data_g <= 'd0;
+        pixel_data_b <= 'd0;
+    end
+    else if(pixel_data_in_vld) begin
+        pixel_data_r <= pixel_data_in_r;
+        pixel_data_g <= pixel_data_in_g;
+        pixel_data_b <= pixel_data_in_b;
+    end
+end
+assign pixel_data_out_r_tmp = ccm_coef_r[0]*pixel_data_r + ccm_coef_r[1]*pixel_data_g + ccm_coef_r[2]*pixel_data_b + ccm_coef_r[3];
+assign pixel_data_out_g_tmp = ccm_coef_g[0]*pixel_data_r + ccm_coef_g[1]*pixel_data_g + ccm_coef_g[2]*pixel_data_b + ccm_coef_g[3];
+assign pixel_data_out_b_tmp = ccm_coef_b[0]*pixel_data_r + ccm_coef_b[1]*pixel_data_g + ccm_coef_b[2]*pixel_data_b + ccm_coef_b[3];
+
+always_ff@(posedge clk or negedge rstn) begin
+    if(~rstn) begin
+        pixel_data_out_r <= 'd0;
+        pixel_data_out_g <= 'd0;
+        pixel_data_out_b <= 'd0;
+    end
+    else if(ccm_en) begin
+        pixel_data_out_r <= pixel_data_out_r_tmp << 10;
+        pixel_data_out_g <= pixel_data_out_g_tmp << 10;
+        pixel_data_out_b <= pixel_data_out_b_tmp << 10;
+    end
+    else begin
+        pixel_data_out_r <= pixel_data_r;
+        pixel_data_out_g <= pixel_data_g;
+        pixel_data_out_b <= pixel_data_b;
+    end
+end
+
+always_ff@(posedge clk or negedge rstn) begin
+    if(~rstn)
+        {pixel_data_out_vld_pre,pixel_data_out_vld} <= 2'd0;
+    else
+        {pixel_data_out_vld_pre,pixel_data_out_vld} <= {pixel_data_in_vld,pixel_data_out_vld_pre};
+end
+
+always_ff@(posedge clk or negedge rstn) begin
+    if(~rstn)
+        h_cnt <= 'd0;
+    else if(ccm_en && pixel_data_in_vld)
+        h_cnt <= (h_cnt==H-1)?  'd0:(h_cnt+1'b1);
+end
+always_ff@(posedge clk or negedge rstn) begin
+    if(~rstn)
+        v_cnt <= 'd0;
+    else if(ccm_en && pixel_data_in_vld && h_cnt==H-1)
+        v_cnt <= (v_cnt==V-1)?  'd0:(v_cnt+1'b1);
+end
+
+always_ff@(posedge clk or negedge rstn) begin
+    if(~rstn)
+        ccm_done <= 1'b0;
+    else if(ccm_en && v_cnt==V-1 && h_cnt==H-1)
+        ccm_done <= 1'b1;
+    else if(ccm_done)
+        ccm_done <= 1'b0;
+end
+endmodule
---
+++ model/ccm.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import numpy as np
+import cv2
+
+class CCM:
+    'Color Correction Matrix'
+
+    def __init__(self, img, ccm):
+        self.img = img
+        self.ccm = ccm
+
+    def execute(self):
+        img_h = self.img.shape[0]
+        img_w = self.img.shape[1]
+        img_c = self.img.shape[2]
+        ccm_img = np.empty((img_h, img_w, img_c), np.uint32)
+        for y in range(img_h):
+            for x in range(img_w):
+                mulval = self.ccm[:,0:3] * self.img[y,x,:]  # ccm的前三列
+                ccm_img[y,x,0] = np.sum(mulval[0]) + self.ccm[0,3]  # ccm的第4列==offset
+                ccm_img[y,x,1] = np.sum(mulval[1]) + self.ccm[1,3]
+                ccm_img[y,x,2] = np.sum(mulval[2]) + self.ccm[2,3]
+                ccm_img[y,x,:] = ccm_img[y,x,:] / 1024
+        self.img = ccm_img.astype(np.uint8)
+        return self.img
+
+ccm = np.zeros((3,4))  
+ccm[0][0] = 1024
+ccm[1][1] = 1024
+ccm[2][2] = 1024
+raw_data = cv2.imread('img_cfa.jpg',cv2.IMREAD_UNCHANGED)
+obj = CCM(raw_data,ccm)
+ccm_data_rgb = obj.execute()
+#cv2.imwrite('bayer_img_cfa.jpg', cfa_data_bayer)
+#cfa_data_rgb = cv2.cvtColor(cfa_data_bayer, cv2.COLOR_BayerRGGB2BGR)
+cv2.imwrite('img_ccm.jpg',ccm_data_rgb)
